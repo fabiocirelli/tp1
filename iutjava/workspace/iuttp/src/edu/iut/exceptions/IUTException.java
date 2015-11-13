c3882482e6157799f5dd9809902204976e957ad5
Aurelien Roy <aurelien.roy@u-psud.fr>
2015-11-13 11:15:30 +0100

    Remplissage de IUTException

--- iutjava/workspace/iuttp/src/edu/iut/exceptions/IUTException.java
+++ iutjava/workspace/iuttp/src/edu/iut/exceptions/IUTException.java
@@ -1,18 +1,23 @@
 package edu.iut.exceptions;
 
-// Exercice 2 
-public class IUTException /* étendre les exceptio, */{
+import edu.iut.app.ApplicationSession;
+
+// Exercice 2
+public class IUTException extends Exception{
 	public IUTException() {
 		super();
+		ApplicationSession.instance().getExceptionLogger().severe("Exception (aucun message spécifié)");
 		// Logger une erreur avec le message empty en utilisant le singleton session, ie le logger défini dans la session */
 
 	}
 	public IUTException(IUTException e) {
 		super (e);
+		ApplicationSession.instance().getExceptionLogger().severe(e.getMessage());
 		// Logger une erreur avec le message contenu dans 'e'  en utilisant le singleton session, ie le logger défini dans la session */
 	}
 	public IUTException(String message) {
 		super(message);
+		ApplicationSession.instance().getExceptionLogger().severe(message);
 		// Logger une erreur avec le message contenu dans 'message'  en utilisant le singleton session, ie le logger défini dans la session */
 	}
 	
